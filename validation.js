4ffe6de033faa994467c831bbb49cc25e4eafdb3
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-21 13:12:11 +0300

    added validations

---
+++ validation.js
@@ -0,0 +1,24 @@
+const validCommands = ["add", "find", "complete", "help", "exit"]
+
+function validateWord(word){
+    if(!word){
+        throw new Error("Missing word argument")
+    }
+}
+
+function validateCommand(command){
+    if(!command){
+        throw new Error("Missing command argument")
+    }
+    if(!validCommands.contains(command.toLowerCase())){
+        throw new Error("Invalid Command")
+    }
+
+}
+
+function validateArgs(word, command){
+    validateWord(word)
+    validateCommand(command)
+}
+
+module.exports = validateArgs()
